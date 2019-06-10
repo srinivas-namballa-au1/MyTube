d6267173c4e6a217012e3d59e8bd6098443eb7d3
Srinivas-Namballa <namballasrinivas902@gmail.com>
2019-06-11 03:51:16 +0530

    is videos loading reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import {createStore, combineReducers} from "redux";
 
 import videosReducer from "./reducers/videosReducer.js";
+import isVideosLoadingReducer from "./reducers/isVideosLoadingReducer.js";
 
 let reducer = combineReducers({
-    videos: videosReducer
+    videos: videosReducer,
+    isVideoLoading: isVideosLoadingReducer
 });
 
 let store = createStore(reducer);
