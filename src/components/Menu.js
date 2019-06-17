ccba75a57d97c01094d039ab1be45f26ab513494
Srinivas-Namballa <namballasrinivas902@gmail.com>
2019-06-17 16:32:46 +0530

    added /app routes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,10 +13,10 @@ class Menu extends React.Component {
                 <ul className="list-group">
                     <li className="list-group-item bg-dark text-white">Menu</li>
                     <li className="list-group-item">
-                        <Link to="/">Trending</Link>    
+                        <Link to="/app">Trending</Link>    
                     </li>
                     <li className="list-group-item">
-                        <Link to="/search">Search</Link>
+                        <Link to="/app/search">Search</Link>
                     </li>
                 </ul>
             </div>
